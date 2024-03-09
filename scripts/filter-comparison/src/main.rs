c1c9a76327aaa8f9cf2b4b6a11e1ca987fa24790
mmta <mmta.gm@gmail.com>
2024-03-10 00:50:11 +0700

    docs: rm inline redundant doc

--- scripts/filter-comparison/src/main.rs
+++ scripts/filter-comparison/src/main.rs
@@ -1,39 +1,3 @@
-/*!
-
-This example provides a way to easily evaluate the performance difference between using rayon vs 
-multiple dedicated threads to execute the hot-code path where events are checked against all directive rules.
-
-At the time of writing, dedicated threads are more performant than rayon for dsiem specific use case, 
-and so it is the one used in directive manager and rule.
-
-Usage:
-
-Activate rayon by setting the environment variable `USE_RAYON` to `true`, or set it
-to `false` or unset to use dedicated threads.
-
-Other environment variables that can be set are:
-- `RAYON_THREAD_POOL_SIZE` to set the number of threads used by rayon
-- `DIRECTIVES_PER_THREAD` to set the number of directives processed by each dedicated thread
-
-After setting the environment variables:
-
-- Put a directive.json file with at least a thousand entries in /configs directory, i.e.:
-  
-  mkdir -p ./target/debug/configs && ln -s path/to/directive.json ./target/debug/configs/directive.json
-
-- Execute one of the following command to start the server (release version is more performant):
-
-  cargo run -p filter-comparison
-  cargo build --release -p filter-comparison && ./target/release/filter-comparison
-
-- Send a POST request using dtester (https://github.com/defenxor/dsiem/tree/master/cmd/dtester):
-
-  ./dtester dsiem -d 192.168.0.1 -f <path-to-directive.json-above> -p 6667 -r [eps]
-
-- Try different values for `eps` and compare the performance between rayon and dedicated threads.
-
-*/
-
 use std::{
     thread::{self, sleep},
     time::Duration,
