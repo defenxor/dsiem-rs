f76b33474c2c4b617e28c2b64928ec2210b29b22
mmta <mmta.gm@gmail.com>
2024-03-12 02:28:51 +0700

    fix: switch to glibc for releases to lower mem usage

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# usage:
+# 1st argument: image name, e.g. defenxor/dsiem-rs
+# 2nd argument: musl (optional) or push (optional)
+# 3rd argument: push (optional), if 2nd argument is musl
+
 dir="./deployments/docker/build/"
 [ ! -e $dir ] && echo must be executed from app root directory. && exit 1
 
 [ -z "$1" ] && echo "1st argument must be the image name, e.g. defenxor/dsiem-rs" && exit 1
+([ "$1" == "musl" ] || [ "$1" == "push" ]) && echo "1st argument must be the image name, e.g. defenxor/dsiem-rs" && exit 1
+
 image_name=$1
+
+[ "$2" == "musl" ] && musl=true && [ "$3" == "push" ] && push_image=true
+[ "$2" == "push" ] && push_image=true
 
 tmpctx=$dir/ctx
 mkdir -p $tmpctx
@@ -22,10 +32,17 @@ done
 
 cd $dir
 
-docker build -f Dockerfile -t $image_name:$version -t $image_name:latest . || exit 1
+[ "$musl" == "true" ] && dockerfile=Dockerfile.musl || dockerfile=Dockerfile
+[ "$musl" == "true" ] && version=${version}-musl
 
-if [ "$2" == "push" ]; then
+echo building $image_name:$version
+
+docker build -f ${dockerfile} -t $image_name:$version . || exit 1
+
+[ "$musl" != "true" ] && docker tag $image_name:$version $image_name:latest
+
+if [ "$push_image" == "true" ]; then
   echo pushing
   docker push $image_name:$version
-  docker push $image_name:latest
+  [ "$musl" != "true" ] && docker push $image_name:latest
 fi
