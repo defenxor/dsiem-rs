034f7b5b32cd693e607f17bf87143e6cf62905bb
mmta <mmta@users.noreply.github.com>
2024-03-24 17:26:08 +0700

    build: add option to use nightly and dynamic linking (#59)

--- scripts/build-glibc.sh
+++ scripts/build-glibc.sh
@@ -6,22 +6,23 @@
 #   scripts/build-glibc.sh
 
 # - build with nightly toolchain and dynamic linking to glibc:
-#   DSIEM_DYNAMIC_LIBC=1 DSIEM_NIGHTLY_RUST=1 scripts/build-glibc.sh
+#   DSIEM_DYNAMIC_LIBGCC=1 DSIEM_NIGHTLY_RUST=1 scripts/build-glibc.sh
 
 # - build specific binary:
 #   scripts/build-glibc.sh --bin dsiem-backend
 
-set -x
-
-dir=$(git rev-parse --show-toplevel) && cd $dir || { echo "not in a git repo" && exit 1; }
+grep -q Dsiem ./Cargo.toml 2>/dev/null || { echo "not in the root of the project" && exit 1; }
 
 # defaults to static build and stable toolchain unless overriden using these
 
-[ "$DSIEM_DYNAMIC_LIBC" ] && target_feature="-crt-static" || target_feature="+crt-static"
+[ "$DSIEM_DYNAMIC_LIBGCC" ] && target_feature="-crt-static" || target_feature="+crt-static"
 [ "$DSIEM_NIGHTLY_RUST" ] &&
   nightly="+nightly" &&
   option_z="-Zlocation-detail=none" &&
-  build_std="-Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort"
+  build_std="-Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort" &&
+  ([ "$DSIEM_DYNAMIC_LIBGCC" ] && option_z="${option_z} -Zsanitizer=address" || true)
+
+set -x
 
 RUSTFLAGS="-C target-feature=${target_feature} ${option_z}" \
   cargo ${nightly} build ${build_std} --target x86_64-unknown-linux-gnu --release $@
