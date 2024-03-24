2e1be8d2552b952bbcd901686cef6a056ed1e7ae
mmta <mmta.gm@gmail.com>
2024-03-24 17:17:16 +0700

    build: add option to use nightly and dynamic linking

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -9,10 +9,16 @@
 # if 2nd argument is the same as env variable DEFAULT_BASE_IMAGE, the image will be tagged as "latest"
 # the default for DEFAULT_BASE_IMAGE for now is "wolfi"
 
+# if env variable DSIEM_DYNAMIC_LIBGCC is set, glibc will be linked dynamically (except for "alpine_musl")
+# if env variable DSIEM_NIGHTLY_RUST is set, the nightly version of rust will be used
+
 dir="./deployments/docker/build"
 [ ! -e $dir ] && echo must be executed from the repo root directory. && exit 1
 
 DEFAULT_BASE_IMAGE=${DEFAULT_BASE_IMAGE:-wolfi}
+
+DSIEM_DYNAMIC_LIBGCC=${DSIEM_DYNAMIC_LIBGCC:-""}
+DSIEM_NIGHTLY_RUST=${DSIEM_NIGHTLY_RUST:-""}
 
 image_name=$1
 base_image=$2
@@ -45,6 +51,8 @@ rsync -vhra --delete ./ $tmpctx/ \
   --exclude='Dockerfile' \
   --exclude-from=<(git -C ./ ls-files --exclude-standard -oi --directory)
 
+mkdir -p $tmpctx/scripts && cp ./scripts/build-glibc.sh $tmpctx/scripts/build-glibc.sh
+
 pkg="$tmpctx/Cargo.toml"
 [ ! -f "$pkg" ] && echo $pkg isnt available && exit 1
 version=$(grep version Cargo.toml | head -1 | cut -d\" -f2)
@@ -52,8 +60,9 @@ version=$(grep version Cargo.toml | head -1 | cut -d\" -f2)
 build_image() {
   local dockerfile=$1
   local image_name=$2
+  local extra_arg=$3
   echo building $image_name ..
-  docker build -f ${dockerfile} -t ${image_name} . || {
+  docker build -f ${dockerfile} -t ${image_name} ${extra_arg} . || {
     echo "failed building image"
     exit 1
   }
@@ -65,10 +74,19 @@ cd $dir/
 build_image 01.webui-builder dsiem-dev/webui-builder
 
 # dsiem-dev/server-builder
-build_image 02.server-builder${dsiem_lib} dsiem-dev/server-builder
+
+[ "$DSIEM_DYNAMIC_LIBGCC" ] &&
+  libgcc_arg="--build-arg apk_extra_packages=libgcc --build-arg DSIEM_DYNAMIC_LIBGCC=1" &&
+  alpine_arg="--build-arg alpine_base=mmta/alpine-glibc" ||
+  alpine_arg="--build-arg alpine_base=alpine"
+
+[ "$DSIEM_NIGHTLY_RUST" ] &&
+  nightly_arg="--build-arg DSIEM_NIGHTLY_RUST=1"
+
+build_image 02.server-builder${dsiem_lib} dsiem-dev/server-builder "${libgcc_arg} ${nightly_arg}"
 
 # dsiem-dev/base-image
-build_image 03.base-${base_image} dsiem-dev/base-image
+build_image 03.base-${base_image} dsiem-dev/base-image "${libgcc_arg} ${alpine_arg}"
 
 this_image=${image_name}:${version}-${base_image}
 
